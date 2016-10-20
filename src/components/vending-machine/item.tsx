78d6e900962edc4d51b384498f76b65145db6a31
David Bohunek <david@bohunek.net>
2016-10-20 21:24:19 +0100

    no ts

--- src/components/vending-machine/item.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-interface Props {
-    imageUrl: string
-}
-
-export default (props: Props) => {
-    return <div className="item" style={{backgroundImage: `url(${props.imageUrl})`}} />;
-}
